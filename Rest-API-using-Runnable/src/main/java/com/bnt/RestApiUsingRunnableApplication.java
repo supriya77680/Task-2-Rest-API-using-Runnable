461658d0cb3dad070c465d2f1bb17aec50c7697e
Supriya Mahajan <mahajansupriya77@gmail.com>
2024-06-26 15:33:36 +0530

    setting up the project

---
+++ Rest-API-using-Runnable/src/main/java/com/bnt/RestApiUsingRunnableApplication.java
@@ -0,0 +1,13 @@
+package com.bnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiUsingRunnableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiUsingRunnableApplication.class, args);
+	}
+
+}
---
+++ Rest-API-using-Runnable/src/test/java/com/bnt/RestApiUsingRunnableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiUsingRunnableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
