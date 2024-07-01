af1d3e3f5bf895197b7d68035f254d8b16acb21f
Supriya Mahajan <mahajansupriya77@gmail.com>
2024-07-01 14:42:04 +0530

    Imlemented test cases

--- Rest-API-using-Runnable/src/main/java/com/bnt/service/EvenOdd.java
+++ Rest-API-using-Runnable/src/main/java/com/bnt/service/EvenOdd.java
@@ -1,32 +1,38 @@
-package com.bnt.service;
+    package com.bnt.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
 
-public class EvenOdd implements Runnable{
+    public class EvenOdd implements Runnable{
 
-    private static final Logger logger = LoggerFactory.getLogger(EvenOdd.class);
+        private static final Logger logger = LoggerFactory.getLogger(EvenOdd.class);
 
-     int number;
+        int number;
+        String result;
 
-  
-    public EvenOdd(int number) {
-        this.number = number;
-    }
-
-    @Override
-public void run() {
-    try {
-        if (number % 2 == 0) {
-            logger.info(number + " is even. (Thread: " + Thread.currentThread().getName() + ")");
-        } else {
-            logger.info(number + " is odd. (Thread: " + Thread.currentThread().getName() + ")");
-        }
-    } catch (ArithmeticException e) {
-        logger.error("ArithmeticException occurred: " + e.getMessage());
-    } catch (Exception e) {
-        logger.error("Exception occurred: " + e.getMessage());
-    }
-  }
-}
     
+        public EvenOdd(int number) {
+            this.number = number;
+        }
+
+        public String getEvenOdd (String result){
+            try {
+                if (number % 2 == 0) {
+                    logger.info(number + " is even. (Thread: " + Thread.currentThread().getName() + ")");
+                } else {
+                    logger.info(number + " is odd. (Thread: " + Thread.currentThread().getName() + ")");
+                }
+            } catch (ArithmeticException e) {
+                logger.error("ArithmeticException occurred: " + e.getMessage());
+            } catch (Exception e) {
+                logger.error("Exception occurred: " + e.getMessage());
+            }
+            return "Result of odd and even is out";
+        }
+
+        @Override
+    public void run() {
+        getEvenOdd(result);
+    }
+    }
+        
